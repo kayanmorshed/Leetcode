90619251b9cc12d545f04ec1f97ebaa23dca5b14
Md Morshed Alam <mm.kayan@gmail.com>
2022-01-15 14:05:04 -0500

    Time: 69 ms (27.32%), Space: 14.8 MB (94.39%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,23 +1,20 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:  
-        # edge cases
-        if nums[-1] < target: return len(nums)
-        if nums[0] > target or not nums: return 0
-        if len(nums) == 1: return 0
+        left, right = -1, len(nums)
         
-        
-        mid = int((len(nums)-1)/2)
-
-        if nums[mid] > target:
-            return self.searchInsert(nums[0:mid+1], target)
-        
-        
-        elif nums[mid] < target:
-            return (mid + 1) + self.searchInsert(nums[mid+1:], target)
-        
-        else:
-            return mid
+        while left < right:
+            mid = (left + right) // 2
+            
+            if mid < 0: return 0
+            
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
     
-        
+        return right
 
         
