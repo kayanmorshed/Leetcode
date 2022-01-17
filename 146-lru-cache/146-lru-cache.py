e339477f12bb84b4a3dc8343424508fb61baa454
Md Morshed Alam <mm.kayan@gmail.com>
2022-01-17 13:07:14 -0500

    Time: 1770 ms (11.80%), Space: 76.2 MB (51.99%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,60 +1,53 @@
-# define a class ListNode to create a doubly linked list
+# definition of a double linked list
 class ListNode:
     def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        
-        # define the pointers
-        self.prev = None
-        self.next = None
+        self.key, self.val = key, val
+        self.prev, self.next = None, None
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
         
-        # add sentinels
-        self.left = ListNode(0,0)
-        self.right = ListNode(0,0)
-        self.left.next = self.right
-        self.right.prev = self.left
-    
-    # insert node at right
+        # create two sentinels and initialize the pointers
+        self.left, self.right = ListNode(0,0), ListNode(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # remove the given 'node' from the linked list
+    def remove_node(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+        
+    # insert the given 'node' to the right side of the linked list
     def insert_node(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
         node.prev, node.next = prev, nxt
     
-    # insert a node from the linked list
-    def remove_node(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
-
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove_node(self.cache[key])
-            self.insert_node(self.cache[key])
+            self.remove_node(self.cache[key]) # remove from the linked list
+            self.insert_node(self.cache[key]) # insert the removed node to the right
             return self.cache[key].val
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        # if key is hit, remove that node        
+        # if hit, remove the node from the list
         if key in self.cache:
             self.remove_node(self.cache[key])
         
-        # update the pointer in the hashmap
-        self.cache[key] =  ListNode(key, value)
+        # update the mapping with the updated/added key-value pair
+        self.cache[key] = ListNode(key, value)
         
-        # insert the node in the linked list
+        # insert the updated node into the list
         self.insert_node(self.cache[key])
         
-        # when the cache is full, remove the LRU
+        # check whether the cache is full; if yes, remove the LRU node
         if len(self.cache) > self.cap:
-            lru = self.left.next
-            self.remove_node(lru) # remove the LRU node from the linked_list
-            del self.cache[lru.key] # remove the mapping of the deleted node from the hashmap 
+            lru_node = self.left.next
+            self.remove_node(lru_node) # remove from the list
+            del self.cache[lru_node.key] # remove the mapping of the lru node
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
