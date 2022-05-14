2597f36b4a4d25b6fc6439ab216ecab17809cf3f
Md Morshed Alam <mm.kayan@gmail.com>
2022-05-13 21:34:57 -0400

    Time: 885 ms (83.13%), Space: 14 MB (62.08%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,25 +1,34 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # get the longest palindrome (from inner to outer)
+        # left and right are the middle indices, when the length of the string is even
+        # left = right = middle index, when the length of the string is odd   
+
+        def getLongestPalidrome(s, left, right): 
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1 : right]                
+        
+
+        # *********** Computation starts here ************
         res = ""
+        
         for idx in range(len(s)):
-            # odd case, like "aba"
-            tmp = self.getLongestPalidrome(s, idx, idx)
-            if len(tmp) > len(res):
-                res = tmp
-                
-            # even case, like "abba"; index = 0,2,4,6
-            tmp = self.getLongestPalidrome(s, idx, idx+1)
-            if len(tmp) > len(res):
-                res = tmp
+            # call the method getLongestPalidrome() thinking 
+            # the current index as middle index
+            
+            # only one middle index (len(s) = odd)
+            returnedPalindrome = getLongestPalidrome(s, idx, idx)  # idx = 1
+            if len(returnedPalindrome) > len(res):
+                res = returnedPalindrome
+            
+            # two middle indices
+            returnedPalindrome = getLongestPalidrome(s, idx, idx+1)  # idx = 1,2
+            if len(returnedPalindrome) > len(res):
+                res = returnedPalindrome             
+
         return res
  
-    # get the longest palindrome (from inner to outer)
-    # left and right are the middle indices, when the length of the string is even
-    # left = right = middle index, when the length of the string is odd   
-    
-    def getLongestPalidrome(self, s, left, right): 
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        return s[left+1 : right]                
+
         
