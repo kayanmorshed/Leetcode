b1e63e5098db1d51c55323631b1d3000ae612416
Md Morshed Alam <mm.kayan@gmail.com>
2022-05-17 03:51:00 -0400

    Time: 1360 ms (16.64%), Space: 28.4 MB (37.20%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -2,6 +2,7 @@ class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
         freq = {}
         
+        # count the frequecy of taksk having same difficulty level
         for task in tasks:
             if task not in freq:
                 freq[task] = 1
@@ -13,10 +14,10 @@ class Solution:
         for key in freq.keys():
             val = freq[key]
             
-            if val == 1:
+            if val == 1: # 1 in freq {} -> output = -1
                 return -1
             
-            res += (val + 2) // 3
+            res += (val + 2) // 3 # that's the ultimate equation
         
         return res
         
