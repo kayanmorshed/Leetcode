dca7ef500529f885bbd14ad8a76dc85f03205d7f
Md Morshed Alam <mm.kayan@gmail.com>
2022-11-13 15:38:21 -0500

    Time: 76 ms (71.73%), Space: 14.3 MB (68.03%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -2,27 +2,42 @@ class Solution:
     def longestValidParentheses(self, s: str) -> int:
         # using dynamic programming (credit: pennlio)
         
-        # add an extra closing parenthesis in the beginning to handle edge case
-        s = ')' + s
-        
-        # Initialize a list of len(s) memory space to trace the longest valied parentheses ending at s[i]  
-        dp = [0 for x in range(len(s))] # dp[i] = longestValidParentheses ending at s[i]
-        mx = 0 # variable to hold the (max) length of the longestValidParentheses so far 
-        
-        for i in range(1,len(s)):
+        # Initialize a list of len(s) memory space to trace the longest valid parentheses ending at s[i]
+        dp_length = [0] * len(s)  # dp_length[i] = length of the longest valid parenthesis string ending at s[i]
+
+        mx = 0  # variable to hold the (max) length of the longest valid parenthesis string so far
+
+        # start from index "1" because the input_string[0] alone cannot form a valid parenthesis string
+        for i in range(1, len(s)):
+            # we are only interested in the closing parenthesis since an opening parenthesis can never complete
+            # the formation of a valid string i.e., a ')' dictates the update of dp_length[i]
+
             if s[i] == ')':
-                # case 1: ()()
-                if s[i-1] == '(': 
-                    dp[i] = dp[i-2] + 2 # add nearest parentheses pairs + 2
-                
-                # case 2: (()) 
-                # i-dp[i-1]-1 is the index of last "(" not paired until this ")"
-                elif i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1] == '(':
-                    if dp[i-1] > 0: # content within current matching pair is valid 
-                    # add nearest parentheses pairs + 2 + parentheses before last "("
-                        dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
+                # ***********************************************************************************
+                # case 1: ()()() => opening and closing parentheses exist in every alternate position
+                # ***********************************************************************************
+
+                if s[i-1] == '(':
+                    # the length of the nearest valid parenthesis string + 2
+                    dp_length[i] = dp_length[i-2] + 2
+
+                # *******************************************************************************************
+                # case 2: ((())) => a series of opening parenthesis and again a series of closing parenthesis
+                # *******************************************************************************************
+
+                # dp_length[i-1] = the length of the nearest valid parenthesis string
+                # (i - dp_length[i-1]) - 1 = the index of last "(" not paired until the current ")"
+
+                elif (i - dp_length[i-1]) - 1 >= 0 and s[i - dp_length[i-1] - 1] == '(':
+                    # check whether the length of the nearest valid parenthesis string is > 0
+                    if dp_length[i-1] > 0:
+                        # length of the nearest valid parenthesis string + 2
+                        # + length of the valid parenthesis string before last unpaired "("
+                        dp_length[i] = dp_length[i-1] + 2 + dp_length[i - dp_length[i-1] - 2]
                     else:
-                    # otherwise is 0
-                        dp[i] = 0
-                mx = max(mx, dp[i])
+                        dp_length[i] = 0 # no consecutive matching '(' and ')'
+
+                # update the length of the longest valid parenthesis string so far
+                mx = max(mx, dp_length[i])
+        
         return mx
